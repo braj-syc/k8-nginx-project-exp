a3cdabcf6d46b0d208c3eca9506b1c39bed098bc
braj-syc <63956838+braj-syc@users.noreply.github.com>
2026-03-28 02:17:57 +0530

    Update load-test.js

--- load-test.js
+++ load-test.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://<your-ingress-url>');
+  http.get('http://20.198.122.246:32129');
   sleep(1);
 }
